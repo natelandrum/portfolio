a8ebaee76887b4b7e82f8f22f9cc58f6af086afa
natelandrum <nate178dawg@gmail.com>
2025-04-16 14:21:51 -0600

    Footer section

---
+++ components/Footer.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  const { t } = useTranslation()
+</script>
+
+<template>
+  <footer class="py-8 px-6">
+        <p>&copy; {{ new Date().getFullYear() }} Nathan Landrum. {{ t('footer.rights') }}</p>
+  </footer>
+</template>
