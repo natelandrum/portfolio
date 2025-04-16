0b323f92279977efa24a563ddcdddee65e447bc7
natelandrum <nate178dawg@gmail.com>
2025-04-16 14:14:51 -0600

    Main app

---
+++ app.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+<template>
+    <div class="flex flex-col items-center">
+      <NavBar />
+      <main>
+        <NuxtPage />
+      </main>
+      <Footer />
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+  const { t } = useTranslation()
+  useHead({
+    title: t('contact.myName') + ' | ' + t('home.title'),
+    meta: [
+      {
+        name: 'description',
+        content: t('home.description'),
+      },
+      {
+        name: 'keywords',
+        content: 'software, developer, web applications, Vue, React, Nuxt.js, Next.js',
+      },
+      {
+        property: 'og:title',
+        content: t('contact.myName') + ' | ' + t('home.title'),
+      },
+      {
+        property: 'og:description',
+        content: t('home.description'),
+      },
+    ],
+  })
+</script>
+
+<template>
+  <div>
+    <Hero />
+    <hr class="border-t border-gray-200">
+    <Project />
+    <hr class="border-t border-gray-200">
+    <Skills />
+    <hr class="border-t border-gray-200">
+    <Contact />
+    <hr class="border-t border-gray-200">
+  </div>
+</template>
